f9513196faa8d7b78f94bc936c6e798c37615709
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-13 14:40:00 +0900

    second commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,11 @@ Rails.application.configure do
 
   ActionCable.server.config.disable_request_forgery_protection = true
   config.action_cable.url = "wss://mini-talk-app-32405.herokuapp.com/cable" 
-  config.action_cable.allowed_request_origins = ['https://mini-talk-app-32405.herokuapp.com', 'http://mini-talk-app-32405.herokuapp.com']  
+  config.action_cable.allowed_request_origins = ['https://mini-talk-app-32405.herokuapp.com', 'http://mini-talk-app-32405.herokuapp.com']
+
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "ws://【Elastic IP】/cable" 
+  config.action_cable.allowed_request_origins = ['http://【Elastic IP】']
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
